9117c0a7ed2aa2fff8c067f4583c6a8af9c4487e
Bruno Soares <vsoares.bruno@gmail.com>
2024-10-04 20:01:23 +0100

    build: setup test and related stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-});
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@services': path.resolve(__dirname, './src/services'),
+    },
+  },
+})
