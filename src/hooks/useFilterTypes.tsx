dbec50ef01e82e48e35048d52663c283a1a24d6c
Bruno Soares <vsoares.bruno@gmail.com>
2024-10-05 16:55:49 +0100

    feat: get pokemon types from api

---
+++ src/hooks/useFilterTypes.tsx
@@ -0,0 +1,55 @@
+import { pokeApiClient } from '@/services/PokeApiClient'
+import { useEffect, useState } from 'react'
+
+interface IFilterTypes {
+  setFilter: (filter: { types: string[] }) => void
+}
+
+export const useFilterTypes = ({ setFilter }: IFilterTypes) => {
+  const [selectedTypes, setSelectedTypes] = useState<string[]>([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [pokemonTypes, setPokemonTypes] = useState<string[]>([])
+  const [error, setError] = useState<string | null>(null)
+  const { fetchAllTypes } = pokeApiClient
+
+  const handleTypeChange = (type: string) => {
+    setSelectedTypes((prevSelectedTypes) =>
+      prevSelectedTypes.includes(type)
+        ? prevSelectedTypes.filter((t) => t !== type)
+        : [...prevSelectedTypes, type]
+    )
+  }
+
+  const clearFilter = () => {
+    setSelectedTypes([]) // Limpa todas as seleções
+  }
+
+  useEffect(() => {
+    setFilter({ types: selectedTypes })
+  }, [selectedTypes])
+
+  useEffect(() => {
+    const fetchPokemonTypes = async () => {
+      setIsLoading(true)
+      try {
+        const fetchedPokemonTypes = await fetchAllTypes()
+        const typesNames = fetchedPokemonTypes.map((pokemon) => pokemon.name)
+        setPokemonTypes(typesNames)
+
+        setIsLoading(false)
+      } catch (error: unknown) {
+        setError(error instanceof Error ? error.message : String(error))
+      }
+    }
+
+    fetchPokemonTypes()
+  }, [])
+  return {
+    isLoading,
+    pokemonTypes,
+    error,
+    selectedTypes,
+    handleTypeChange,
+    clearFilter,
+  }
+}
